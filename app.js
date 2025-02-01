e9ffdf06f822887f1b8634bc085d3d4b2e621050
dieandrde <diegoe.andrade05@gmail.com>
2025-02-01 02:48:38 -0300

    Se añade el texto ingresado a la lista vacía, en caso de no ingresar nada, la página muestra un mensaje de alerta

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+
+function agregarAmigo() {
+    let input = document.getElementById('amigo');
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, ingrese un nombre.");
+        return;
+    }
+
+    listaNombres.push(nombre);
+    input.value = ""; //limpia el input
+    console.log(listaNombres)
+}
