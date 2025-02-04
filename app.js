fad86b9619d17935864b79a2ad238822e76e5cf6
dieandrde <diegoe.andrade05@gmail.com>
2025-02-04 01:00:53 -0300

    se crea una función que permite que cada elemento de la lista de amigos, sea agregado en un <li> en la parte inferior de la página

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaNombres = [];
+let amigos = [];
 
 function agregarAmigo() {
     let input = document.getElementById('amigo');
-    let nombre = input.value.trim();
+    let nombre = input.value.trim(); //valor sin espacios
 
     if (nombre === "") {
-        alert("Por favor, ingrese un nombre.");
+        alert("Por favor, inserte un nombre.");
         return;
     }
 
-    listaNombres.push(nombre);
+    amigos.push(nombre);
     input.value = ""; //limpia el input
-    console.log(listaNombres)
+    console.log(amigos)
+
+    agregarLista();
 }
+
+function agregarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++ ) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i]; //agrega los nombres al li
+        lista.appendChild(li);
+    }
+
+}
+
+
+
+
+
